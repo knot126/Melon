3c1e105c7f5e003f036eea6744aa531154ddd577
knot126 <73200930+knot126@users.noreply.github.com>
2023-01-14 23:42:50 +0000

    New licence

--- src/window.c
+++ src/window.c
@@ -1,6 +1,13 @@
 /**
- * Copyright (C) 2021 - 2022 Knot126 and Descentix Software
- * ========================================================
+ * Copyright (C) 2021 - 2023 Knot126
+ * 
+ * It is against the licence terms of this software to use it or it's source code
+ * as input for training a machine learning model, or in the development of a
+ * machine learning model. If you have found this text as the output of a machine
+ * learning algorithm, please report it both your software vendor and to the
+ * developers of the software at [https://github.com/knot126/Melon/issues].
+ * 
+ * =============================================================================
  * 
  * Windowing access
  * 
