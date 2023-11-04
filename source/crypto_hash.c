7075d83aeb3ee908b40cca5915c9500a8291bbf1
knot126 <73200930+knot126@users.noreply.github.com>
2023-11-04 18:51:32 +0000

    ok it worked ?

--- source/crypto_hash.c
+++ source/crypto_hash.c
@@ -39,70 +39,58 @@ static void DgCryptoCubeHashRound(uint32_t * const block) {
 	for (size_t i = 0; i <= 0b1111; i++) {
 		block[0b10000 | i] += block[i];
 	}
-	DgLog(DG_LOG_VERBOSE, "1. %s", DgStringEncodeBase16(128, block));
 	
 	// 2. Rotate x[0jklm] upwards by 7 bits, for each (j,k,l,m). 
 	for (size_t i = 0; i <= 0b1111; i++) {
 		block[i] = ROTL(block[i], 7);
 	}
-	DgLog(DG_LOG_VERBOSE, "2. %s", DgStringEncodeBase16(128, block));
 	
 	// 3. Swap x[00klm] with x[01klm], for each (k,l,m). 
 	for (size_t i = 0; i <= 0b111; i++) {
 		SWAP(uint32_t, block[i], block[0b1000 | i]);
 	}
-	DgLog(DG_LOG_VERBOSE, "3. %s", DgStringEncodeBase16(128, block));
 	
 	// 4. Xor x[1jklm] into x[0jklm], for each (j,k,l,m). 
 	for (size_t i = 0; i <= 0b1111; i++) {
 		block[i] ^= block[0b10000 | i];
 	}
-	DgLog(DG_LOG_VERBOSE, "4. %s", DgStringEncodeBase16(128, block));
 	
 	// 5. Swap x[1jk0m] with x[1jk1m], for each (j,k,m). 
 	for (size_t i = 0; i <= 0b11; i++) {
 		for (size_t j = 0; j <= 0b1; j++) {
 			size_t k = (i << 2) | j;
-			// DgLog(DG_LOG_VERBOSE, "5. 0x%x = (0x%x << 2) | 0x%x", k, i, j);
 			SWAP(uint32_t, block[0b10000 | k], block[0b10010 | k]);
 		}
 	}
-	DgLog(DG_LOG_VERBOSE, "5. %s", DgStringEncodeBase16(128, block));
 	
 	// 6. Add x[0jklm] into x[1jklm] modulo 2^32, for each (j,k,l,m). 
 	for (size_t i = 0; i <= 0b1111; i++) {
 		block[0b10000 | i] += block[i];
 	}
-	DgLog(DG_LOG_VERBOSE, "6. %s", DgStringEncodeBase16(128, block));
 	
 	// 7. Rotate x[0jklm] upwards by 11 bits, for each (j,k,l,m). 
 	for (size_t i = 0; i <= 0b1111; i++) {
 		block[i] = ROTL(block[i], 11);
 	}
-	DgLog(DG_LOG_VERBOSE, "7. %s", DgStringEncodeBase16(128, block));
 	
 	// 8. Swap x[0j0lm] with x[0j1lm], for each (j,l,m). 
 	for (size_t i = 0; i <= 0b1; i++) {
 		for (size_t j = 0; j <= 0b11; j++) {
 			size_t k = (i << 3) | (j);
-			// DgLog(DG_LOG_VERBOSE, "8. 0x%x = (0x%x << 3) | 0x%x", k, i, j);
 			SWAP(uint32_t, block[k], block[0b100 | k]);
 		}
 	}
-	DgLog(DG_LOG_VERBOSE, "8. %s", DgStringEncodeBase16(128, block));
 	
 	// 9. Xor x[1jklm] into x[0jklm], for each (j,k,l,m). 
 	for (size_t i = 0; i <= 0b1111; i++) {
 		block[i] ^= block[0b10000 | i];
 	}
-	DgLog(DG_LOG_VERBOSE, "9. %s", DgStringEncodeBase16(128, block));
 	
 	// 10. Swap x[1jkl0] with x[1jkl1], for each (j,k,l). 
 	for (size_t i = 0; i <= 0b111; i++) {
 		size_t j = i << 1;
 		SWAP(uint32_t, block[0b10000 | j], block[0b10001 | j]);
 	}
-	DgLog(DG_LOG_VERBOSE, "10. %s", DgStringEncodeBase16(128, block));
 }
 
 #undef SWAP
@@ -143,15 +131,13 @@ DgError DgCryptoCubeHasherInit(DgCryptoCubeHasher *this, uint32_t i, uint32_t r,
 	this->state[0b00001] = b;
 	this->state[0b00010] = r;
 	
-	// HACK memleak
-	// DgLog(DG_LOG_VERBOSE, "%s", DgStringEncodeBase16(32 * 4, this->state));
-	
 	// Preform the initial i rounds
 	for (size_t x = 0; x < i; x++) {
 		DgCryptoCubeHashRound(this->state);
-		// HACK memleak
-		// DgLog(DG_LOG_VERBOSE, "%s", DgStringEncodeBase16(32 * 4, this->state));
 	}
+	
+	/// @see https://github.com/parabirb/cubehash/blob/main/test.js
+	// DgLog(DG_LOG_VERBOSE, "IV: %s", DgStringEncodeBase16(32 * 4, this->state));
 	
 	return DG_ERROR_SUCCESS;
 }
